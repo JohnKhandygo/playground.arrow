9ac6b7783b54fbbcb4db729f2f99536c5a4f885c
Evgenii Khandygo <johnkhandygo@gmail.com>
2022-08-30 23:17:00 +0200

    init: spring initializr + .gitignore.

---
+++ src/main/kotlin/playground/arrow/ArrowApplication.kt
@@ -0,0 +1,11 @@
+package playground.arrow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArrowApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArrowApplication>(*args)
+}
